f4fbd057a960c57ef87c1e636140b8ff5c91c170
kotableOrKisaki <super_misha05@mail.ru>
2022-08-24 20:00:46 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace firstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            double jumpForce = 15.7;
+            float movementSpeed = 9.6f;
+            char space = ' ';
+            string characterName = "exampleName";
+            bool isAlive = true;
+            decimal xSpawnPoint = 765.6945m;
+            uint startCoins = 100;
+            short maxDamage = 9000;
+            byte locationLevel = 0;
+        }
+    }
+}
